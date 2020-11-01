9cf0d97d42e6dd75c538edbddc0fb63acba9e028
Dhivya-prabha <49254873+Dhivya-prabha@users.noreply.github.com>
2020-11-01 10:06:17 +0530

    Add files via upload

---
+++ practice/Draggable.java
@@ -0,0 +1,33 @@
+package sample;
+
+import java.util.Arrays;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+class Draggable {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://jqueryui.com/draggable/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,400)");
+		driver.switchTo().frame(0);
+		
+		WebElement drag = driver.findElementById("draggable");
+		Actions act = new Actions(driver);
+
+		act.dragAndDropBy(drag, 100, 75).perform();
+		;
+
+	}
+
+}
